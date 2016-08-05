2de4f9b54a1f2ffd6f4047069a35dba40f9283bf
Selwyn <talk@selwyn.cc>
2016-08-05 14:24:40 +0200

    Add comment handling tests

--- tests/test.js
+++ tests/test.js
@@ -46,11 +46,53 @@ var sortOrderTests = [
   }
 ];
 
+var commentOrderTests = [
+  {
+    message: 'Keep comment intact.',
+    fixture: 'a{flex: 0;/*flex*/}',
+    expected: 'a{flex: 0;/*flex*/}'
+  },
+  {
+    message: 'Keep dangling comment intact.',
+    fixture: 'a{flex: 0;\n/*end*/}',
+    expected: 'a{flex: 0;\n/*end*/}'
+  },
+  {
+    message: 'Keep multiple comments intact.',
+    fixture: 'a{flex: 0;\n/*flex*/\n/*flex 2*/}',
+    expected: 'a{flex: 0;\n/*flex*/\n/*flex 2*/}'
+  },
+  {
+    message: 'Keep newline comment above declaration.',
+    fixture: 'a{flex: 0;\n/*border*/\nborder: 0;}',
+    expected: 'a{\n/*border*/\nborder: 0;flex: 0;}'
+  },
+  {
+    message: 'Keep inline comment beside declaration.',
+    fixture: 'a{flex: 0;\nborder: 0; /*border*/}',
+    expected: 'a{\nborder: 0; /*border*/flex: 0;}'
+  }
+];
+
 test('Should order CSS declarations.', function (t) {
   // Set amount of assertions by setting two assertions per sort order test
   t.plan(sortOrderTests.length * 2);
 
   sortOrderTests.forEach(function (test) {
+    var options = test.options || {};
+
+    processTest(test.fixture, options).then(function (result) {
+      t.equal(result.css, test.expected, test.message);
+      t.equal(result.warnings().length, 0);
+    });
+  });
+});
+
+test('Should retain comments.', function (t) {
+  // Set amount of assertions by setting two assertions per sort order test
+  t.plan(commentOrderTests.length * 2);
+
+  commentOrderTests.forEach(function (test) {
     var options = test.options || {};
 
     processTest(test.fixture, options).then(function (result) {
