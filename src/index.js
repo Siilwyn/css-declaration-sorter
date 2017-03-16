7e9dc4b036e29373ad8eea96272b8183bac14409
Efremov Alexey <lexich121@gmail.com>
2017-03-17 00:19:53 +0300

    Put declarations before nested selector
    Resolves issue #23

--- src/index.js
+++ src/index.js
@@ -10,21 +10,20 @@ const timsort = require('timsort').sort;
 function sortCssDecls (cssDecls, sortOrder) {
   if (sortOrder === 'alphabetically') {
     timsort(cssDecls, function (a, b) {
-      if (a.prop !== b.prop) {
-        return a.prop < b.prop ? -1 : 1;
+      if (a.type === 'decl' && b.type === 'decl') {
+        return comparator(a.prop, b.prop);
       } else {
-        return 0;
+        return compareDifferentType(a, b);
       }
     });
   } else {
     timsort(cssDecls, function (a, b) {
-      const aIndex = sortOrder.indexOf(a.prop);
-      const bIndex = sortOrder.indexOf(b.prop);
-
-      if (aIndex !== bIndex) {
-        return aIndex < bIndex ? -1 : 1;
+      if (a.type === 'decl' && b.type === 'decl') {
+        const aIndex = sortOrder.indexOf(a.prop);
+        const bIndex = sortOrder.indexOf(b.prop);
+        return comparator(aIndex, bIndex);
       } else {
-        return 0;
+        return compareDifferentType(a, b);
       }
     });
   }
@@ -69,7 +68,6 @@ function processCss (css, sortOrder) {
           node.remove();
         }
       }
-
       return;
     }
 
@@ -121,3 +119,11 @@ module.exports = postcss.plugin('css-declaration-sorter', function (options) {
     });
   };
 });
+
+function comparator (a, b) {
+  return a === b ? 0 : a < b ? -1 : 1;
+}
+
+function compareDifferentType (a, b) {
+  return (a.type === 'decl') ? -1 : (b.type === 'decl') ? 1 : 0;
+}
