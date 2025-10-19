76bf718884fa6cd9eed4aa3e944bfa9ff26f345d
Selwyn <talk@selwyn.cc>
2025-10-19 12:08:12 +0200

    Add excluded and stabilized properties to keep up with changes

--- src/property-scraper.mjs
+++ src/property-scraper.mjs
@@ -1,34 +1,49 @@
 import fs from 'node:fs/promises';
 
-const { css } = await fs.readFile(new URL(import.meta.resolve('@mdn/browser-compat-data')))
+const { css } = await fs.readFile(
+  new URL(import.meta.resolve('@mdn/browser-compat-data')),
+)
   .then(JSON.parse);
 
-const isStandardProperty = (name) => (property) => Boolean(
-  property.__compat?.status.standard_track === true &&
-  property.__compat?.status.experimental === false &&
-  property.__compat?.status.deprecated === false &&
-  (
-    property.__compat?.mdn_url &&
-    !property.__compat?.mdn_url.includes('Guide') &&
-    [...property.__compat.mdn_url.split('/')].pop() === name
-  ),
+const isStandardProperty = (name) => (property) => (
+  Boolean(
+    property.__compat?.status.standard_track === true &&
+    property.__compat?.status.experimental === false &&
+    property.__compat?.status.deprecated === false &&
+    (
+      property.__compat?.mdn_url &&
+      !property.__compat?.mdn_url.includes('Guide') &&
+      [...property.__compat.mdn_url.split('/')].pop() === name
+    ),
+  )
 );
 
-const cssProperties = Object.entries({ ...css.properties, ...css['at-rules']['font-face'] })
-  .filter(([name, data]) =>
-    isStandardProperty (name) (data) ||
-    Object.values(data).some(isStandardProperty(name)),
-  )
-  .map(([name]) => name)
+const excludedProperties = ['WOFF'];
+const stabilizedProperties = [
+  'font-stretch',
+  'font-synthesis-position',
+  'font-variant',
+];
+
+const scrapedProperties = Object.entries({
+  ...css.properties,
+  ...css['at-rules']['font-face'],
+})
+  .filter(([name]) => !excludedProperties.includes(name))
+  .filter(([name, data]) => (
+    isStandardProperty(name)(data) ||
+    Object.values(data).some(isStandardProperty(name))
+  ))
+  .map(([name]) => name);
+
+const cssProperties = [...scrapedProperties, ...stabilizedProperties]
   .sort((nameA, nameB) => (
-    nameA === 'all'
-      ? -1
-      : nameA.localeCompare(nameB)
+    nameA === 'all' ? -1 : nameA.localeCompare(nameB)
   ));
 
 fs.writeFile(
   'src/orders/alphabetical.mjs',
   `export const properties = ${
     JSON.stringify(cssProperties, null, 2).replaceAll('"', "'")
-  }\n`,
+  };\n`,
 );
